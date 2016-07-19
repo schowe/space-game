d7a777525e1e443983b31e2e27197ca812d9b478
cebremer <cbremer@uos.de>
2016-07-19 11:12:30 +0200

    Ich hab committet du affe

--- app/core.js
+++ app/core.js
@@ -6,7 +6,10 @@ var container;
 
 var camera, scene, renderer;
 
+
+
 init();
+
 animate();
 
 function init() {
@@ -42,73 +45,33 @@ function init() {
 
     //
 
-    object = new THREE.Mesh( new THREE.SphereGeometry( 75, 20, 10 ), material );
-    object.position.set( -400, 0, 200 );
-    scene.add( object );
 
-    object = new THREE.Mesh( new THREE.IcosahedronGeometry( 75, 1 ), material );
-    object.position.set( -200, 0, 200 );
-    scene.add( object );
+    //sph = new THREE.Mesh( new THREE.SphereGeometry(50,20,20),material);
+    //sph.position.set(0,0,0);
+    //scene.add(sph);
 
-    object = new THREE.Mesh( new THREE.OctahedronGeometry( 75, 2 ), material );
-    object.position.set( 0, 0, 200 );
-    scene.add( object );
 
-    object = new THREE.Mesh( new THREE.TetrahedronGeometry( 75, 0 ), material );
-    object.position.set( 200, 0, 200 );
-    scene.add( object );
+    var player = Player();
+    player.init();
+    var world = World();
+    world.init();
+    var movement = Movement();
+    movement.init();
+
+    
+
 
     //
 
-    object = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100, 4, 4 ), material );
-    object.position.set( -400, 0, 0 );
-    scene.add( object );
 
-    object = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100, 4, 4, 4 ), material );
-    object.position.set( -200, 0, 0 );
-    scene.add( object );
 
-    object = new THREE.Mesh( new THREE.CircleGeometry( 50, 20, 0, Math.PI * 2 ), material );
+    object = new THREE.AxisHelper( 100 );
     object.position.set( 0, 0, 0 );
     scene.add( object );
 
-    object = new THREE.Mesh( new THREE.RingGeometry( 10, 50, 20, 5, 0, Math.PI * 2 ), material );
-    object.position.set( 200, 0, 0 );
-    scene.add( object );
-
-    object = new THREE.Mesh( new THREE.CylinderGeometry( 25, 75, 100, 40, 5 ), material );
-    object.position.set( 400, 0, 0 );
-    scene.add( object );
-
-    //
-
-    var points = [];
-
-    for ( var i = 0; i < 50; i ++ ) {
-
-        points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * Math.sin( i * 0.1 ) * 15 + 50, ( i - 5 ) * 2 ) );
-
-    }
-
-    object = new THREE.Mesh( new THREE.LatheGeometry( points, 20 ), material );
-    object.position.set( -400, 0, -200 );
-    scene.add( object );
-
-    object = new THREE.Mesh( new THREE.TorusGeometry( 50, 20, 20, 20 ), material );
-    object.position.set( -200, 0, -200 );
-    scene.add( object );
-
-    object = new THREE.Mesh( new THREE.TorusKnotGeometry( 50, 10, 50, 20 ), material );
-    object.position.set( 0, 0, -200 );
-    scene.add( object );
-
-    object = new THREE.AxisHelper( 50 );
-    object.position.set( 200, 0, -200 );
-    scene.add( object );
-
-    object = new THREE.ArrowHelper( new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 0 ), 50 );
+   /** object = new THREE.ArrowHelper( new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 0 ), 50 );
     object.position.set( 400, 0, -200 );
-    scene.add( object );
+    scene.add( object ); */
 
     //
 
@@ -147,19 +110,12 @@ function render() {
 
     var timer = Date.now() * 0.0001;
 
-    camera.position.x = Math.cos( timer ) * 800;
-    camera.position.z = Math.sin( timer ) * 800;
+   // camera.position.x = Math.cos( timer ) * 800;
+   // camera.position.z = Math.sin( timer ) * 800;
 
     camera.lookAt( scene.position );
 
-    for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
 
-        var object = scene.children[ i ];
-
-        object.rotation.x = timer * 5;
-        object.rotation.y = timer * 2.5;
-
-    }
 
     renderer.render( scene, camera );
 
