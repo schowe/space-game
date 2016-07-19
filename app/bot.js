995a59c07f9339c3a72908f7e3d5a0d3f55b3d84
Felix Schneppe <fschneppe@uos.de>
2016-07-19 09:34:40 +0200

    extend the bot wrapper class
    - add Asteroid and Enemy class
    - add empty functions shoot and move

--- app/bot.js
+++ app/bot.js
@@ -1,34 +1,51 @@
 // Botklasse
-
-var direction;
-var speed;
-var location;
-
-bot = function(d,s,l) {
-    direction = d;
-    speed = s;
-    location = l;
+function Bot(direction,speed,location) {
+    this.direction = direction;
+    this.speed = speed;
+    this.location = location;
 }
 
-move: function(delta, asteroids, enemies) {
-    // Setze direction neu
+Bot.prototype.move = function(delta, asteroids, enemies) {
 
-    // ausweichen
+};
 
-    // allen Asteroiden
-    for(asteroid of asteroids) {
 
-    }
+// Asteroidenklasse
+function Asteroid(direction,speed,location) {
+    Bot.call(direction,speed,location);
+};
 
-    // sowie allen Schiffen
-    for(enemy of enemies) {
+Asteroid.prototype = Object.create(Bot.prototype);
+Asteroid.prototype.constructor = Asteroid;
 
-    }
+// Enemyklasse
+function Enemy(direction,speed,location) {
+    Bot.call(direction,speed,location);
+};
+
+Enemy.prototype = Object.create(Bot.prototype);
+Enemy.prototype.constructor = Enemy;
+
+Enemy.prototype.shoot = function() {
+
+};
+
+
+// aktualisiere Position der Asteroiden und Gegner
+// Setze direction neu
+function updateLocation(delta) {
+    // 1. Schritt: ideale Richtung ausrechnen
+
+    // 2. Schritt: Asteroiden und Gegner sortieren
+
+    // 3. Schritt: Ausweichen
+    // Asteroiden haben keine Intelligenz -> Bewegung behalten
+    // Gegner sind intelligent -> allen vor sie liegenden Asteroiden ausweichen
+    //                         -> allen vor sie liegenden Gegnern ausweichen
+    // -> vordere updaten und Richtung des nächsten anhand der neuen Position
+    //    ausrechnen
+
 
 
     location = location + direction;
-}
-
-shoot: function() {
-
 }
