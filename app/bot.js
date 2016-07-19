3108e17c0a77d5375bd150254c05390a2de8b3a9
Felix Schneppe <fschneppe@uos.de>
2016-07-19 13:40:12 +0200

    set the update-method and complete the avoidance movement

--- app/bot.js
+++ app/bot.js
@@ -1,6 +1,6 @@
 // Botklasse
 var minObstacleDistance = 100;
-var enemy, asteroid, playerPosition;
+var enemy, asteroid, playerPosition, shootAble;
 
 // Sortierfunktion für Bots (Enemies und Asteroids)
 // je naeher am Schiff, desto niedriger der Indize
@@ -36,6 +36,7 @@ function Enemy(speed,location, weapon) {
     this.location = location;
     this.weapon = weapon;
     this.isAlive = true;
+    this.shootAble = false;
 };
 
 Enemy.prototype.move = function(delta, asteroids, enemies) {
@@ -54,23 +55,37 @@ Enemy.prototype.move = function(delta, asteroids, enemies) {
     var ShipCollisions = raycaster.intersectObjects(enemies,true);
 
     // falls es zu einer Moeglichkeit einer Kollision kommt, aendere Richtung
-    asteroid = AsteroidCollisions.length > 0 ? AsteroidCollisions[0] : null;
-    enemy   =  ShipCollisions.length > 0 ? ShipCollisions[0] : null;
+    asteroid = AsteroidCollisions.length > 0 ? AsteroidCollisions[0] : Infinity;
+    enemy   =  ShipCollisions.length > 0 ? ShipCollisions[0] : Infinity;
 
-    // nur dem ersten Gegenstand ausweichen
-    if(enemy.distanceToSquared(this.position) <
-        asteroid.distanceToSquared(this.position)) {
-            wrongDir = enemy.direction;
+    if(asteroid!=Infinity || enemy!=Infinity) {
+        // nur dem ersten Gegenstand ausweichen
+        if(enemy.distanceToSquared(this.position) <
+            asteroid.distanceToSquared(this.position)) {
+                wrongDir = enemy.direction;
+        } else {
+                wrongDir = asteroid.direction;
+        }
+
+        // Ausweichrichtung:
+        // negative Hindernisflugrichtung, bis zu 90° Winkel um ideale Richtung
+        // gedreht
+        avoidDir = new Vector3(0,0,0);
+        avoidDir.sub(wrongDir);
+        alpha = Math.PI * Math.random() - Math.PI/2;
+        avoidDir.applyAxisAngle(directionToPlayer,alpha);
+
+        avoidDir.normalize();
+
+        // je naeher an Gegenstand desto mehr ausweichen
+        avoidImpact = ??
     } else {
-            wrongDir = asteroid.direction;
+        avoidDir = new Vector3(0,0,0);
+        avoidImpact = 0;
+        shootAble = true;
     }
 
-    // Ausweichrichtung:
-    // negative Hindernisflugrichtung, im 90° Winkel um ideale Richtung gedreht
-    avoidDir = new Vector3(0,0,0);
-    avoidDir.sub(wrongDir);
-    alpha = Math.PI * Math.random() - Math.PI/2;
-    avoidDir.applyAxisAngle(directionToPlayer,alpha);
+    direction = directionToPlayer.normalize() + avoidImpact * avoidDir();
 
     // normalisiere, um Geschwindigkeit nur von speed abhaengig zu machen
     direction.normalize();
@@ -89,9 +104,9 @@ Enemy.prototype.shot = function() {
 }
 
 
-// Kollisionsueberpruefung
-function checkCollisions() {
-
+// Kollisionsueberpruefung und getroffene Ausschalten
+function checkCollisionAndDestroy() {
+    return enemies;
 }
 
 // aktualisiere Position der Asteroiden und Gegner
@@ -125,9 +140,18 @@ function updateLocation(delta) {
 }
 
 function update(delta) {
+    // Spielerposition updaten
     playerPosition = new THREE.Vector3(0,0,0);
+    // Gegner und Asteroiden updaten
     updateLocation(delta);
-    checkCollision(asteroids, enemies);
+    // Kollisionsueberpruefung -> zerstoerte Loeschen
+    enemies = checkCollisionAndDestroy(asteroids, enemies);
+    // Schiessen
+    for(enemy of enemies) {
+        if(enemy.shootAble==true) {
+            enemy.shoot();
+        }
+    }
 }
 
 function initAI(level) {
