395d959b3020abb64697bb8ac7f59363c9cb2dca
Varaion <daltenhoff@uos.de>
2016-07-20 10:57:11 +0200

    fixes missing renderer

--- app/core.js
+++ app/core.js
@@ -13,6 +13,53 @@ function init() {
     document.body.appendChild( container );
 
 
+    
+    
+    
+    // Beispiel-Code ...
+    
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.y = 400;
+
+    scene = new THREE.Scene();
+
+    var light, object;
+
+    scene.add( new THREE.AmbientLight( 0x404040 ) );
+
+    light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 0, 1, 0 );
+    scene.add( light );
+
+    /*var map = new THREE.TextureLoader().load( 'textures/UV_Grid_Sm.jpg' );
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 16;
+
+    var material = new THREE.MeshLambertMaterial( { map: map, side: THREE.DoubleSide } );*/
+    var material = new THREE.MeshBasicMaterial();
+
+    //
+
+
+    //sph = new THREE.Mesh( new THREE.SphereGeometry(50,20,20),material);
+    //sph.position.set(0,0,0);
+    //scene.add(sph);
+
+    object = new THREE.AxisHelper( 100 );
+    object.position.set( 0, 0, 0 );
+    scene.add( object );
+
+   /** object = new THREE.ArrowHelper( new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 0 ), 50 );
+    object.position.set( 400, 0, -200 );
+    scene.add( object ); */
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+
 
     // TODO: scene code goes here
     // Welt erzeugen
@@ -20,6 +67,7 @@ function init() {
     // Gegner erzeugen
     // ...
     // => Funktionen aus anderen Dateien laden!!
+
 
 
 
